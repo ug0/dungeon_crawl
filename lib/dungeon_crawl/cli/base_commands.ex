6d9b8c474f9351bb4326d797cf29920ae5d0da2d
ug0 <ug03706@gmail.com>
2018-05-26 13:17:00 +0800

    Handle invalid input

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,6 +1,34 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  def ask_for_option(options) do
+    answer =
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt()
+
+    with {option, _} <- Integer.parse(answer),
+         chosen when chosen != nil <- Enum.at(options, option - 1) do
+      chosen
+    else
+      :error -> retry(options)
+      _ -> retry(options)
+    end
+  end
+
+  def retry(options) do
+    display_error("Invalid option")
+    ask_for_option(options)
+  end
+
+  def display_error(message) do
+    Shell.cmd("clear")
+    Shell.error(message)
+    Shell.prompt("Press Enter to continue.")
+    Shell.cmd("clear")
+  end
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
@@ -14,10 +42,5 @@ defmodule DungeonCrawl.CLI.BaseCommands do
   def generate_question(options) do
     options = Enum.join(1..Enum.count(options), ",")
     "Wich one? [#{options}]\n"
-  end
-
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
   end
 end
--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -7,14 +7,9 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Shell.info("Start by choosing your hero:")
 
     heroes = DungeonCrawl.Heroes.all()
-    find_hero_by_index = &Enum.at(heroes, &1)
 
     heroes
-    |> display_options()
-    |> generate_question
-    |> Shell.prompt()
-    |> parse_answer
-    |> find_hero_by_index.()
+    |> ask_for_option
     |> confirm_hero
   end
 
